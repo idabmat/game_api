c4044f413749c9f720d964a194ea80210cec3e6c
Igor de Alcantara Barroso <idabmat@gmail.com>
2020-05-03 23:29:57 -0300

    Lint

--- test/yatzy_api/web/router_test.exs
+++ test/yatzy_api/web/router_test.exs
@@ -9,22 +9,21 @@ defmodule YatzyApi.Web.RouterTest do
 
   test "returns ok on /" do
     assert capture_log(fn ->
-      conn = conn(:get, "/") |> Router.call(@opts)
+             conn = conn(:get, "/") |> Router.call(@opts)
 
-      assert conn.state == :sent
-      assert conn.status == 200
-      assert conn.resp_body == "ok"
-    end) =~ "GET /"
-
+             assert conn.state == :sent
+             assert conn.status == 200
+             assert conn.resp_body == "ok"
+           end) =~ "GET /"
   end
 
   test "return 404 elsewhere" do
     assert capture_log(fn ->
-      conn = conn(:get, "/pokemon") |> Router.call(@opts)
+             conn = conn(:get, "/pokemon") |> Router.call(@opts)
 
-      assert conn.state == :sent
-      assert conn.status == 404
-      assert conn.resp_body == "Not found"
-    end) =~ "GET /pokemon"
+             assert conn.state == :sent
+             assert conn.status == 404
+             assert conn.resp_body == "Not found"
+           end) =~ "GET /pokemon"
   end
 end
