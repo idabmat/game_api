94951739c733e8d87fe705890857097eb71b6014
Igor de Alcantara Barroso <igor@talimhq.com>
2020-05-03 23:42:49 -0300

    Setup GH actions on PRs (#1)
    * Setup GH actions on PRs
    
    * Rename yml file
    
    * Lint
    
    * Capture log in tests
    
    * Lint

--- test/yatzy_api/web/router_test.exs
+++ test/yatzy_api/web/router_test.exs
@@ -2,23 +2,28 @@ defmodule YatzyApi.Web.RouterTest do
   use ExUnit.Case, async: true
   use Plug.Test
 
+  import ExUnit.CaptureLog
+
   alias YatzyApi.Web.Router
   @opts Router.init([])
 
   test "returns ok on /" do
-    conn =
-      conn(:get, "/")
-      |> Router.call(@opts)
+    assert capture_log(fn ->
+             conn = conn(:get, "/") |> Router.call(@opts)
 
-    assert conn.state == :sent
-    assert conn.status == 200
-    assert conn.resp_body == "ok"
+             assert conn.state == :sent
+             assert conn.status == 200
+             assert conn.resp_body == "ok"
+           end) =~ "GET /"
   end
 
   test "return 404 elsewhere" do
-    conn = conn(:get, "/pokemon") |> Router.call(@opts)
-    assert conn.state == :sent
-    assert conn.status == 404
-    assert conn.resp_body == "Not found"
+    assert capture_log(fn ->
+             conn = conn(:get, "/pokemon") |> Router.call(@opts)
+
+             assert conn.state == :sent
+             assert conn.status == 404
+             assert conn.resp_body == "Not found"
+           end) =~ "GET /pokemon"
   end
 end
