7fbb00d3d651a53fafd11e536a13f2643b658157
Igor de Alcantara Barroso <idabmat@gmail.com>
2020-05-17 01:27:42 -0300

    Remove ueberauth from extra applications

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule GameApi.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :ueberauth],
+      extra_applications: [:logger],
       mod: {GameApi, []}
     ]
   end
