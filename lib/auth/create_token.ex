83024d6921ee960ac9b0a8f7158273cce02aaf2b
Igor de Alcantara Barroso <idabmat@gmail.com>
2020-05-18 23:21:18 -0300

    Replace unecessary protocol by module

--- lib/auth/create_token.ex
+++ lib/auth/create_token.ex
@@ -1,21 +1,15 @@
-defprotocol Auth.CreateToken do
-  @fallback_to_any true
+defmodule Auth.CreateToken do
+  @moduledoc false
+
+  alias Auth.Account
 
   @spec execute(any(), token_gateway: module()) :: {:ok, String.t()} | {:error, atom()}
-  def execute(account, options \\ [])
-end
-
-defimpl Auth.CreateToken, for: Any do
-  def execute(_account, _options), do: {:error, :account_not_found}
-end
-
-defimpl Auth.CreateToken, for: Auth.Account do
-  def execute(account, token_gateway: token_gateway) do
+  def execute(%Account{} = account, token_gateway: token_gateway) do
     case token_gateway.set(account) do
       token when is_binary(token) -> {:ok, token}
       _ -> {:error, :account_not_found}
     end
   end
 
-  def execute(any, options), do: Auth.CreateToken.Any.execute(any, options)
+  def execute(_account, _options), do: {:error, :account_not_found}
 end
